7f9b96b1923741cbad27ba06d67e10aa3cb12c81
Leandro97ramos <leandro.97.ramos@gmail.com>
2024-07-30 19:02:38 -0300

    Fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EstudianteComponent } from './estudiante/estudiante.component';
 import { MateriaComponent } from './materia/materia.component';
-
+import { HomeComponent } from './home/home.component';
 //rutas home - estudiantes - materias - Gestion
 const routes: Routes = [
+
+  { path: 'home', component: HomeComponent },
   
 //estudiantes
 
@@ -12,6 +14,9 @@ const routes: Routes = [
 
 //materia
 { path: 'materia', component: MateriaComponent },
+{ path: '', redirectTo: 'home', pathMatch: 'full' },
+
+{ path: '**', redirectTo: 'home', pathMatch: 'full' },
 
 ];
 @NgModule({
